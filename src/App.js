56bce285b5f48009d6a5f59cb06fe7fa8c9bc9c2
Banji Zillight <banjiolawolez@gmail.com>
2023-04-22 14:00:17 +0100

    useState() & mapping through each element

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import React, {useState} from 'react';
+import boxes from "./boxes"
 
 function App() {
+  const [squares, setSquares] = useState(boxes)
+
+  const boxElements = squares.map(box => (
+    <div className="boxes" key={box.id}></div>
+  ))
   return (
     <div className="App">
-      
+      {boxElements}
     </div>
   );
 }
